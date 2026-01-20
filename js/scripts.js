8ded28fe12b94e227aed55bca5e1baaf3c26a0f7
Artem Norenko <norenko.artem@gmail.com>
2026-01-20 09:55:07 +0200

    06 final

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
- function showTab(tabId) {
+        function showTab(tabId) {
             const tabs = document.querySelectorAll('#output .tab-content');
             tabs.forEach(tab => tab.style.display = 'none');
             document.getElementById(tabId).style.display = 'block';
+            document.querySelectorAll('.left-nav a').forEach(link => {
+                link.classList.remove('isActive');
+            });
+            document.querySelector(`.left-nav a[onclick="showTab('${tabId}')"]`).classList.add('isActive');
+        }
+
+        function navMenuClickItem(event) {
+            event.preventDefault();
         }
